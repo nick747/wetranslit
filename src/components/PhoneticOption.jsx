5174aeea382a4d23f4ddbe404dc5e9a7a2159f67
nick747 <nickodon747@proton.me>
2024-06-15 15:29:20 +0200

    fix to the options [not showing the number]

--- src/components/PhoneticOption.jsx
+++ src/components/PhoneticOption.jsx
@@ -1,14 +1,12 @@
 import "../App.css";
-const PhoneticOption = ({ choice, index, onChoiceClick }) => {
+const PhoneticOption = ({ choice, onChoiceClick }) => {
   return (
     <div
       id="phonetic_option"
       className="flex gap-1 h-full justify-center items-center p-1 opacity-50 w-32 cursor-pointer"
       onClick={() => onChoiceClick(choice)}
     >
-      <p className="normal font-bold">
-        <span className="opacity-50">{index + 1}.</span> {choice}
-      </p>
+      <p className="normal font-bold">{choice}</p>
     </div>
   );
 };
--- src/components/PhoneticOptions.jsx
+++ src/components/PhoneticOptions.jsx
@@ -7,11 +7,7 @@ const PhoneticOptions = ({ choices, onChoiceClick }) => {
       className="flex w-full h-12 bg-[#161616] text-white shadow-[0_0_10px_rgba(0,0,0,0.25)]"
     >
       {choices.map((choice, index) => (
-        <PhoneticOption
-          choice={choice}
-          index={index}
-          onChoiceClick={onChoiceClick}
-        />
+        <PhoneticOption choice={choice} onChoiceClick={onChoiceClick} />
       ))}
     </div>
   );
