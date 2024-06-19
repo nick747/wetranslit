8dbc7f7291c4ae7ee002c56030338804fc1dfe63
nick747 <nickodon747@proton.me>
2024-06-19 21:59:07 +0200

    let phonetic options expand to fill option-area

--- src/components/PhoneticOption.jsx
+++ src/components/PhoneticOption.jsx
@@ -3,7 +3,7 @@ const PhoneticOption = ({ choice, onChoiceClick }) => {
   return (
     <div
       id="phonetic_option"
-      className="flex gap-1 h-full justify-center items-center p-1 opacity-50 w-32 cursor-pointer"
+      className="flex gap-1 grow h-full justify-center items-center p-1 opacity-50 w-32 cursor-pointer"
       onClick={() => onChoiceClick(choice)}
     >
       <p className="normal font-bold">{choice}</p>
