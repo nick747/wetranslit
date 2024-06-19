c141470c9500a5b10b1b638025628e1f1fe89863
nick747 <nickodon747@proton.me>
2024-06-19 21:45:17 +0200

    add wrap to phonetic table

--- src/components/PhoneticTable.jsx
+++ src/components/PhoneticTable.jsx
@@ -3,7 +3,10 @@ import dictionary from "../logic/dictionary";
 
 const PhoneticTable = () => {
   return (
-    <div id="phonetic_table" className="my-4 flex justify-center items-center">
+    <div
+      id="phonetic_table"
+      className="my-4 flex justify-center items-center flex-wrap"
+    >
       {Object.entries(dictionary).map(([key, valueArray]) => (
         <PhoneticElement russian={key} arr={valueArray} />
       ))}
