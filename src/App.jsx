1b685bd74a99d2153ca35220f916f5f6504a34bb
nick747 <nickodon747@proton.me>
2024-06-20 11:55:49 +0200

    delete footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import Header from "./components/header";
-import Footer from "./components/footer";
 import Content from "./components/content";
 import "./App.css";
 
 function App() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="w-full h-screen flex flex-col justify-center items-center">
       <div id="app" className="h-screen w-3/4 py-24">
         <Header />
         <Content />
-        <Footer />
       </div>
     </div>
   );
